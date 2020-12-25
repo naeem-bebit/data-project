77b9fb6060f38a771889879a757b9a66d9aa08dd
naeem_hussien <naeem.hussien@bebit.com>
2020-12-25 23:45:07 +0900

    template

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 """Main."""
-from flask import Flask
+from flask import Flask, render_template
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,7 +12,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    title = "Home Page"
+    programming_languages = [
+        "Python",
+        "Java",
+        "Javascript"
+    ]
+    return render_template('index.html', title=title, programming_languages=programming_languages)
 
 
 if __name__ == "__main__":
